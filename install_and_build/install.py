a87b65d2bc166ba6580d940c676dfd68040df217
SuppliedOrange <70258998+SuppliedOrange@users.noreply.github.com>
2023-11-04 17:17:01 +0000

    Update install.py

--- install_and_build/install.py
+++ install_and_build/install.py
@@ -3,12 +3,15 @@ import sys
 
 # Get the major and minor numbers of the current python version
 
-PYTHON_VER = dict( zip( ("major", "minor"), tuple( sys.version_info ) [ 0 : 2 ] ) )
+PYTHON_VER = dict( zip( ("major", "minor", "patch"), tuple( sys.version_info ) [ 0 : 3 ] ) )
 
 # If the python version is 3.12 or more, back out.
 
 if PYTHON_VER['major'] >= 3 and PYTHON_VER['minor'] >= 12:
     raise Exception("Your python version must not exceed 3.11 due to compatibility issues with bottle.py (https://github.com/bottlepy/bottle/issues/1430)")
+
+if PYTHON_VER['major'] == 3 and PYTHON_VER["minor"] == 10 and PYTHON_VER["patch"] == 0:
+    raise Exception("Your python is 3.10.0 which is known to have issues with pyinstaller (https://github.com/pyinstaller/pyinstaller/issues/6301).")
 
 # Install the dependencies
 print("Installing dependencies")
