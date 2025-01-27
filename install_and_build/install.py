00ab8f9458c8dca6fc03ed9519be84061138475d
SuppliedOrange <70258998+SuppliedOrange@users.noreply.github.com>
2025-01-27 13:05:03 +0530

    remove v3.12+ restrictions while installing.

--- install_and_build/install.py
+++ install_and_build/install.py
@@ -5,10 +5,10 @@ import sys
 
 PYTHON_VER = dict( zip( ("major", "minor", "patch"), tuple( sys.version_info ) [ 0 : 3 ] ) )
 
-# If the python version is 3.12 or more, back out.
-
+''' # Fixed now.
 if PYTHON_VER['major'] >= 3 and PYTHON_VER['minor'] >= 12:
     raise Exception("Your python version must not exceed 3.11 due to compatibility issues with bottle.py (https://github.com/bottlepy/bottle/issues/1430)")
+'''
 
 if PYTHON_VER['major'] == 3 and PYTHON_VER["minor"] == 10 and PYTHON_VER["patch"] == 0:
     raise Exception("Your python is 3.10.0 which is known to have issues with pyinstaller (https://github.com/pyinstaller/pyinstaller/issues/6301).")
